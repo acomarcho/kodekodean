6c5721b444489c30da9952a83bd0abfed96d78aa
acomarcho <acoxstpd@gmail.com>
2023-06-06 16:51:51 +0700

    Feat: Initialize register page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'kodekodean.id',
+  title: 'kodekodean.id - Belajar pemrograman',
   description: 'Belajar pemrograman ala anak ITB',
 }
 
---
+++ components/register/register.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+
+export default function Register() {
+  return (
+    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:items-center min-h-screen lg:px-[2.5rem] lg:py-[1rem]">
+      <div className="flex flex-col gap-[1rem] lg:w-[50%]">
+        <div className="flex flex-col gap-[1rem]">
+          <h1 className="text-white text-[1.25rem] font-bold lg:text-[3rem] leading-[3.625rem]">
+            <span className="text-yellow">Buat</span> akun baru
+          </h1>
+          <p className="text-light-gray text-[1rem] lg:text-[1.25rem]">
+            Yuk, buat akun di <span className="text-green">kodekodean.id</span>{" "}
+            untuk mulai belajar!
+          </p>
+          <p className="text-light-gray text-[1rem] lg:text-[1.25rem]">
+            Sudah punya akun? <span className="text-cyan underline">Masuk</span>
+          </p>
+        </div>
+        {/* TODO: Form */}
+      </div>
+      <div className="hidden lg:block lg:w-[50%]">
+        <Image src="/assets/join-us.png" alt="Join us" width={316} height={579} className="mx-auto" />
+      </div>
+    </div>
+  );
+}
