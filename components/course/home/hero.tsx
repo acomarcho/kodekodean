084dab608251a33ec35d109ade70242e5db026f1
acomarcho <acoxstpd@gmail.com>
2023-06-17 21:56:14 +0700

    Feat: Adjust padding @ hero for mobile view

--- components/course/home/hero.tsx
+++ components/course/home/hero.tsx
@@ -5,7 +5,7 @@ export default function Courses() {
   return (
     <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between min-h-screen lg:p-[2.5rem]">
       <div className="flex flex-col gap-[1rem] lg:w-[50%]">
-        <div className="flex flex-col gap-[1rem]">
+        <div className="flex flex-col gap-[0rem] lg:gap-[1rem]">
           <h1 className="text-white text-[1.25rem] font-bold lg:text-[3rem] leading-[3.625rem]">
             Ingin <span className="text-purple">belajar apa</span> Anda hari
             ini?
