314d1e3c490cccdc611545147f85c6eb5b1d2269
acomarcho <acoxstpd@gmail.com>
2023-06-17 23:02:27 +0700

    Feat: Slice courses section

--- app/course/page.tsx
+++ app/course/page.tsx
@@ -1,5 +1,6 @@
 import Wrapper from "@/components/common/wrapper";
 import Hero from "@/components/course/home/hero";
+import Courses from "@/components/course/home/courses";
 import AuthProvider from "@/components/common/auth-provider";
 
 export const metadata = {
@@ -11,7 +12,10 @@ export default async function RegisterPage() {
   return (
     <Wrapper>
       <AuthProvider requireLogin={true} redirectTo={"/login"}>
-        <Hero />
+        <div className="min-h-screen">
+          <Hero />
+          <Courses />
+        </div>
       </AuthProvider>
     </Wrapper>
   );
--- components/course/home/hero.tsx
+++ components/course/home/hero.tsx
@@ -3,7 +3,7 @@ import Greeting from "./greeting";
 
 export default function Courses() {
   return (
-    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between min-h-screen lg:p-[2.5rem]">
+    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:p-[2.5rem]">
       <div className="flex flex-col gap-[1rem] lg:w-[50%]">
         <div className="flex flex-col gap-[0rem] lg:gap-[1rem]">
           <h1 className="text-white text-[1.25rem] font-bold lg:text-[3rem] leading-[3.625rem]">
