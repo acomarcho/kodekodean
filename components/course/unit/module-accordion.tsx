bf277651ddedc24f896f26cc1d4d48ac5c87c617
acomarcho <acoxstpd@gmail.com>
2023-06-19 16:46:24 +0700

    Feat: Finish accordion slice

--- components/course/unit/module-accordion.tsx
+++ components/course/unit/module-accordion.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 
 interface ModuleProps {
@@ -17,16 +18,30 @@ export default function ModuleAccordion({ module }: ModuleProps) {
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const handleClick = () => {
+    setIsOpen(oldIsOpen => !oldIsOpen)
+  }
+
   return (
     <div className="p-[1rem] bg-dark-gray lg:p-[1.5rem]" key={id}>
-      <h1 className="text-white text-[1rem] font-bold lg:text-[2rem]">
-        <span className={`${finished ? "text-green" : "text-yellow"}`}>
-          {`#${rank}`}.{" "}
-        </span>
-        {title}
-      </h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-white text-[1rem] font-bold w-[60%] lg:text-[2rem]">
+          <span className={`${finished ? "text-green" : "text-yellow"}`}>
+            {`#${rank}`}.{" "}
+          </span>
+          {title}
+        </h1>
+        <button className={`pointer transition-all ${isOpen ? 'rotate-180' : 'rotate-0'}`} onClick={handleClick}>
+          <Image
+            src="/icons/chevron-down.png"
+            alt="Chevron icon"
+            width={25}
+            height={25}
+          />
+        </button>
+      </div>
       <div
-        className={`transition-all flex flex-col gap-[0.75rem] lg:gap-[1rem]`}
+        className={`transition-all flex flex-col gap-[0.75rem] overflow-hidden ${isOpen ? 'max-h-[10rem] mt-[1rem]' : 'max-h-[0rem]'} lg:gap-[1rem]`}
       >
         <p className="text-light-gray text-[1rem] lg:text-[1.25rem]">
           {description}
