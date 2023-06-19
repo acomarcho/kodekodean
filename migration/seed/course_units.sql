5aa3937ed126b04567a0a9728ea34801f68aba15
acomarcho <acoxstpd@gmail.com>
2023-06-19 13:13:36 +0700

    Feat: Add seed for course_units table

---
+++ migration/seed/course_units.sql
@@ -0,0 +1,7 @@
+INSERT INTO course_units (title, course_id) VALUES
+  ('Paradigma fungsional dan Haskell', 1),
+  ('Rekursi sederhana pada Haskell', 1),
+  ('Pemrosesan list pada Haskell', 1),
+  ('Fungsi lambda dan fungsi sebagai parameter', 1),
+  ('Sample unit #1 untuk course 2', 2),
+  ('Sample unit #2 untuk course 2', 2);
