48e5b726de7895831e6ce4aa11083ed6a09ccd71
acomarcho <acoxstpd@gmail.com>
2023-06-20 20:34:53 +0700

    Feat: Add desktop view navbar

--- components/course/module/desktop-view.tsx
+++ components/course/module/desktop-view.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import Image from "next/image";
+import { useContext } from "react";
+import { UnitModuleContext } from "@/contexts/unit-module-context";
+
 export default function DesktopView() {
-  return <h1>Desktop view!</h1>
+  const { unitModule, chunks } = useContext(UnitModuleContext);
+
+  return (
+    <>
+      {/* Navbar */}
+      <div className="fixed top-0 left-0 right-0 bg-dark-gray">
+        <div className="max-w-[1280px] mx-auto p-[1rem] px-[2.5rem]">
+          <div className="flex items-center gap-[2rem]">
+            <button className="pointer">
+              <Image
+                src="/icons/xmark.png"
+                alt="X mark"
+                width={38}
+                height={51}
+              />
+            </button>
+            <h1 className="text-white font-bold text-[2rem]">{`${unitModule.rank}. ${unitModule.title}`}</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
