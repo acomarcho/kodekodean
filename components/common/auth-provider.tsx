a23cad17e98501fad1fbb1a0903e672d284e6501
acomarcho <acoxstpd@gmail.com>
2023-06-20 13:24:06 +0700

    Feat: Replace router.push with router.replace at auth-provider.tsx

--- components/common/auth-provider.tsx
+++ components/common/auth-provider.tsx
@@ -39,13 +39,13 @@ export default function AuthProvider({
         if (user) {
           setUser(user);
           if (requireGuest) {
-            router.push(redirectTo!);
+            router.replace(redirectTo!);
           } else {
             setIsLoading(false);
           }
         } else {
           if (requireLogin) {
-            router.push(redirectTo!);
+            router.replace(redirectTo!);
           } else {
             setIsLoading(false);
           }
