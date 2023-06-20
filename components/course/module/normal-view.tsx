f8355de66f4a7222bddb21e4718b996f955d525a
acomarcho <acoxstpd@gmail.com>
2023-06-20 19:13:18 +0700

    Feat: Initialize react-markdown

--- components/course/module/normal-view.tsx
+++ components/course/module/normal-view.tsx
@@ -3,10 +3,26 @@
 import Image from "next/image";
 import { useContext, useState } from "react";
 import { UnitModuleContext } from "@/contexts/unit-module-context";
+import ReactMarkdown from "react-markdown";
+import rehypeRaw from "rehype-raw";
 
 export default function NormalView() {
   const { unitModule, chunks } = useContext(UnitModuleContext);
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+  const content = `# 1. Pendahuluan
+  
+  ## Video
+  
+  <iframe src="https://www.youtube.com/embed/RBqWwm2eEq0"/>
+  
+  ## Teks
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat, lectus quis suscipit dapibus, nibh mi ali quam arcu, in commodo sapien enim vitae magna. Quisque dictum egestas est. Ut molestie est libero, sed egestas leo viverra et. Sed tristique, velit congue pharetra posuere, nulla elit luctus lorem, at lacinia sapien urna ut diam. Quisque nunc sapien, mattis eget dolor in, pulvinar rhoncus dolor. Aliquam id aliquam libero, vel feugiat lectus. Etiam aliquet, massa vel molestie cursus, velit elit rutrum diam, et scelerisque urna orci a ipsum. Praesent ultricies mi nec odio semper faucibus. 
+  
+  ![Test image](https://upload.wikimedia.org/wikipedia/commons/2/29/Postgresql_elephant.svg)
+  
+  Nulla diam nunc, pulvinar nec hendrerit sed, dignissim non ipsum. Fusce porta lacus at cursus lacinia. Nulla facilisi. Morbi vitae condimentum quam. Etiam at egestas massa, blandit consectetur elit. Pellentesque condimentum posuere vestibulum. Praesent aliquam ac libero at laoreet.  
+  `;
 
   return (
     <>
@@ -59,6 +75,10 @@ export default function NormalView() {
             </div>
           </div>
         </div>
+      </div>
+      {/* Content */}
+      <div className="p-[1rem]">
+        <ReactMarkdown rehypePlugins={[rehypeRaw]}>{content}</ReactMarkdown>
       </div>
     </>
   );
