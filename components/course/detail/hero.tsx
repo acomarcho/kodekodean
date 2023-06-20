c7855e3e7856dbb1dc9104a8b0793411a763b203
acomarcho <acoxstpd@gmail.com>
2023-06-21 01:11:29 +0700

    Fix: Pad heroes

--- components/course/detail/hero.tsx
+++ components/course/detail/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   const course = useContext(CourseContext);
 
   return (
-    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:items-center lg:p-[2.5rem]">
+    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:items-center lg:p-[2.5rem] pt-[76px] lg:pt-[82px]">
       <div className="flex flex-col gap-[1rem] lg:w-[50%]">
         <div className="flex flex-col gap-[0rem] lg:gap-[1rem]">
           <h1 className="text-white text-[1.25rem] font-bold lg:text-[3rem] lg:leading-[3.625rem]">
