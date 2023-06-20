43f7d27f79c4c6e2fcf5391adab002cbc3500185
acomarcho <acoxstpd@gmail.com>
2023-06-20 17:32:39 +0700

    Feat: Add navigation bar at normal view

--- components/course/module/normal-view.tsx
+++ components/course/module/normal-view.tsx
@@ -1,3 +1,28 @@
+"use client";
+
+import Image from "next/image";
+import { useContext } from "react";
+import { UnitModuleContext } from "@/contexts/unit-module-context";
+
 export default function NormalView() {
-  return <h1>Normal view!</h1>
+  const unitModule = useContext(UnitModuleContext);
+
+  return (
+    <>
+      {/* Navigation bar */}
+      <div className="p-[1rem] bg-dark-gray flex justify-between items-center">
+        <button>
+          <Image
+            src="/icons/hamburger.png"
+            alt="Hamburger icon"
+            width={32}
+            height={27.75}
+          />
+        </button>
+        <h1 className="w-[60%] text-white text-right font-bold text-[1.25rem]">
+          {`${unitModule.rank}. ${unitModule.title}`}
+        </h1>
+      </div>
+    </>
+  );
 }
