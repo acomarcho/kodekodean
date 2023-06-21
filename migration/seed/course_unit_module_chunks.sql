2204fd5ef4437d837b41ce4729800bca35159999
acomarcho <acoxstpd@gmail.com>
2023-06-21 19:17:21 +0700

    Feat: Update DB seed

--- migration/seed/course_unit_module_chunks.sql
+++ migration/seed/course_unit_module_chunks.sql
@@ -1,2 +1,4 @@
 INSERT INTO course_unit_module_chunks (title, content_path, rank, unit_module_id) VALUES
-  ('Pendahuluan', '/content/dev/test.md', 1, 1);
+  ('Pendahuluan', '/content/dev/test.md', 1, 1),
+  ('Perbedaan dengan paradigma fungsional', '/content/dev/test.md', 2, 1),
+  ('Kelebihan paradigma fungsional', '/content/dev/test.md', 3, 1);
