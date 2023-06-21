78bb61136c73d8a11409ed0a59b4c42539468e7d
acomarcho <acoxstpd@gmail.com>
2023-06-21 11:18:03 +0700

    Feat: Add API to get unit module information

---
+++ app/api/unit-module/chunks/[id]/route.ts
@@ -0,0 +1,46 @@
+import { NextResponse } from "next/server";
+import conn from "@/lib/pg";
+import { QueryResult } from "pg";
+
+interface Params {
+  params: {
+    id: string;
+  };
+}
+
+export async function GET(_: Request, { params }: Params) {
+  try {
+    let query: string;
+    let values: number[];
+    let result: QueryResult<any>;
+
+    if (!/^\d+$/.test(params.id)) {
+      return NextResponse.json(
+        { message: "Format ID tidak valid" },
+        { status: 400 }
+      );
+    }
+
+    query =
+      "SELECT id, title, content_path, rank, unit_module_id FROM course_unit_module_chunks WHERE unit_module_id = $1 ORDER BY rank ASC";
+    values = [parseInt(params.id)];
+    result = await conn!.query(query, values);
+
+    if (result.rows.length === 0) {
+      return NextResponse.json(
+        {
+          message:
+            "Unit module tidak ditemukan atau unit module tidak memiliki chunk",
+        },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json({ moduleChunks: result.rows }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Terjadi kesalahan pada server" },
+      { status: 500 }
+    );
+  }
+}
