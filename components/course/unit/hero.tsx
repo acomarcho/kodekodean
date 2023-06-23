d2c75f8c0ade69fc83e7ac90fa5520c90bee71be
acomarcho <acoxstpd@gmail.com>
2023-06-23 13:52:28 +0700

    Fix: Add extra 1rem to top paddings

--- components/course/unit/hero.tsx
+++ components/course/unit/hero.tsx
@@ -8,7 +8,7 @@ export default function Hero() {
   const courseUnit = useContext(CourseUnitContext);
 
   return (
-    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:items-center lg:p-[2.5rem] pt-[76px] lg:pt-[82px]">
+    <div className="flex flex-col gap-[1rem] p-[1rem] lg:flex-row lg:justify-between lg:items-center lg:p-[2.5rem] pt-[calc(76px+1rem)] lg:pt-[calc(82px+1rem)]">
       <div className="flex flex-col gap-[1rem] lg:w-[50%]">
         <div className="flex flex-col gap-[0rem] lg:gap-[1rem]">
           <h1 className="text-white text-[1.25rem] font-bold lg:text-[3rem] lg:leading-[3.625rem]">
